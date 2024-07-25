6c874ad6b66849c0d6dc0715889ca1580fe4a2e9
P-Hoffmann98 <patrick.hoffmann1998@yahoo.de>
2024-07-25 16:36:30 +0200

    updated landing page with a controls explanation

--- classes/drawable-object.class.js
+++ classes/drawable-object.class.js
@@ -38,9 +38,9 @@ class DrawableObject {
       this instanceof Endboss
     ) {
       ctx.beginPath();
-      ctx.lineWidth = "5";
-      ctx.strokeStyle = "blue";
-      ctx.rect(this.x, this.y, this.width, this.height);
+      // ctx.lineWidth = "5";
+      // ctx.strokeStyle = "blue";
+      // ctx.rect(this.x, this.y, this.width, this.height);
       ctx.stroke();
     }
   }
